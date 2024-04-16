ef4801f305cacaec870cfef746f581db0ed0c3e9
Nae Pongsatorn <pongsatornnea@gmail.com>
2024-04-16 23:18:59 +0700

    fix some text

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,8 @@
     <router-link to="/axiosTest">axiosTest</router-link>
     <router-link to="/crud">CRUD</router-link>
     <router-link to="/qrPayment">qrPayment</router-link>
+    
     <h1>Pongsatorn Tassaro</h1>
-    <h1>Nae and Nut </h1>
-    <h1>OHAYOOOO</h1>
     <h1>Nathawadee Hnakna</h1>
   </nav>
   <router-view/>
